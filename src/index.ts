18448087469291111ca6d12eba88fcb3261432f6
Faizan Khan <faizankhan062002@gmail.com>
2026-03-30 20:37:58 +0530

    chore: remove cors middleware from express application

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import cors from 'cors';
 import { ENV } from './config/env.config';
 import { initDb } from './config/db.config';
 import logger from './config/logger';
@@ -14,7 +13,6 @@ import pricingRoutes from './routes/pricing.routes';
 const app = express();
 
 // Middleware
-app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
